e3d2e9c9818c484e35abfac86352809adf2c30f9
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-18 11:32:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,4 +24,14 @@ message.author.send(`**مدة الرابط : يـوم
 
 });
 
+client.on('ready',async () => {
+  sendReady('491517470138433536', `**__ تـم تـشـغـيـل الـبـوت بـنـجـاح , Link Bot | Owner Bot : SoM # 1100__**`);
+  
+  function sendReady(channel, message) {
+    client.channels.get(channel).send(message);
+    console.log(message);
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
